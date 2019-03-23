3b4614dfdd259cb38893433849ab8b310a3e6743
Natasha Chernis <natashachernis@gmail.com>
2019-03-23 22:15:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,7 @@ require 'pry'
   #   }
   # }
 
+
 #def remove_strawberry(contacts)
 #  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_if {|flavor| flavor == "strawberry"}
 #  contacts
